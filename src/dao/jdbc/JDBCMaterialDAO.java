077f18206e3a52e6425567b24808bc100dfb43fd
Marco Antônio <marcoantonio.tecinf@gmail.com>
2016-10-18 19:13:57 -0400

    Atualizando JDBCMaterialDAO gerenciado pelo Hibernate

--- src/dao/jdbc/JDBCMaterialDAO.java
+++ src/dao/jdbc/JDBCMaterialDAO.java
@@ -1,77 +1,68 @@
 package dao.jdbc;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-import conexao.ConnectionFactory;
+import java.util.List;
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import dao.dao.MaterialDAO;
 import model.Material;
 
 public class JDBCMaterialDAO implements MaterialDAO {
 	
-	private Connection connection;
+	private SessionFactory factory;
 
 	public JDBCMaterialDAO() {
-		connection = ConnectionFactory.getConnection();
+		
+		factory = HibernateStarter.getSessionFactory();
 	}
 
 	@Override
 	public void inserir(Material material) {
-		try {
-			String SQL = "INSERT INTO material values (?, ?, ?, ?)";
-			PreparedStatement ps = connection.prepareStatement(SQL);
-
-			ps.setInt(1, material.getId());
-			ps.setString(2, material.getNome());
-			ps.setDouble(3, material.getPeso());
-			ps.setLong(4, material.getTelefone());
-			ps.executeUpdate();
-
-			SQL = "UPDATE vendedor_fornecedor SET id_fornecedor = ? where id = ?";
-			PreparedStatement ps2 = connection.prepareStatement(SQL);
-			fornecedor.getVendedores().forEach(vendedorFornecedor -> {
-
-				try {
-					ps2.setInt(1, fornecedor.getId());
-					ps2.setInt(2, vendedorFornecedor.getId());
-					ps2.executeUpdate();
-				} catch (Exception e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-
-			});
-
-		} catch (SQLException ex) {
-			Logger.getLogger(JDBCClienteDAO.class.getName()).log(Level.SEVERE, null, ex);
-		}
+		Session session = factory.getCurrentSession();
+		session.beginTransaction();
+		session.save(material);
+		session.getTransaction().commit();
 	}
 
 	@Override
 	public void remover(int id) {
-		// TODO Auto-generated method stub
-		
+		Session session = factory.getCurrentSession();
+		session.beginTransaction();
+		Material material = new Material();
+		material.setId(id);
+		session.delete(material);
+		session.getTransaction().commit();		
 	}
 
 	@Override
 	public List<Material> listar() {
-		// TODO Auto-generated method stub
-		return null;
+		Session session = factory.getCurrentSession();
+		session.beginTransaction();
+		@SuppressWarnings("unchecked")
+		List<Material> materiais = session.createQuery("from Material").getResultList();
+		session.getTransaction().commit();
+		return materiais;
 	}
 
 	@Override
 	public Material buscar(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		Session session = factory.getCurrentSession();
+		session.beginTransaction();
+		Material material = new Material();
+		material.setId(id);
+		material = session.get(Material.class, material.getId());
+		session.getTransaction().commit();
+		return material;
 	}
 
 	@Override
 	public void editar(Material material) {
-		// TODO Auto-generated method stub
+		Session session = factory.getCurrentSession();
+		session.beginTransaction();
+		session.update(material);
+		session.getTransaction().commit();
 		
 	}
 
