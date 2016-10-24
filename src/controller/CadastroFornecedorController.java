a5641ce29a4bc9a0981694b523e91b21239847ec
Marco Antônio <marcoantonio.tecinf@gmail.com>
2016-10-24 18:21:22 -0400

    Arrumando funcionalidades na Tela de Cadastro de Fornecedor

--- src/controller/CadastroFornecedorController.java
+++ src/controller/CadastroFornecedorController.java
@@ -1,6 +1,5 @@
 package controller;
 
-
 import java.io.IOException;
 
 import app.AlertaApp;
@@ -15,82 +14,113 @@ import javafx.stage.Stage;
 import model.Fornecedor;
 
 public class CadastroFornecedorController {
-	
+
 	JDBCFornecedorDAO fornecedorDAO = new JDBCFornecedorDAO();
+
+	@FXML
+	private TextField txtId;
+
+	@FXML
+	private TextField txtRazaoSocial;
+
+	@FXML
+	private TextField txtTelefone;
+
+	@FXML
+	private TextField txtCnpj;
+
+	@FXML
+	private TextField txtEmail;
+
+	@FXML
+	void salvar(ActionEvent event) throws Exception {
+		if (txtId.getText().isEmpty()) {
+			Fornecedor fornecedor = new Fornecedor();
+			fornecedor = montaFornecedor();
+			fornecedorDAO.inserir(fornecedor);
+			new AlertaApp().start(new Stage(), "Salvo com Sucesso!");
+			txtId.setText(Integer.toString(fornecedor.getId()));
+
+		} else {
+			Fornecedor fornecedor = montaFornecedor();
+			fornecedorDAO.editar(fornecedor);
+			new AlertaApp().start(new Stage(), "Alterado com Sucesso!");
+			posicionaFornecedor(fornecedor);
+		}
+
+	}
+
+	@FXML
+	void novo(ActionEvent event) {
+		limpaCampos();
+	}
+
+	@FXML
+	void excluir(ActionEvent event) throws Exception {
+		if(!txtId.getText().isEmpty()){
+			fornecedorDAO.remover(Integer.parseInt(txtId.getText()));
+			new AlertaApp().start(new Stage(), "Excluído com Sucesso!");
+			limpaCampos();
+		}else{
+			new AlertaApp().start(new Stage(), "Selecione um Fornecedor para excluir.");
+		}
+	}
+
+	@FXML
+	void pesquisar(ActionEvent event) throws Exception {
+		Stage stageTelaPesquisa = new Stage();
+		stageTelaPesquisa.initModality(Modality.APPLICATION_MODAL);
+		new TelaPesquisaApp().start(stageTelaPesquisa);
+		if (TelaPesquisaController.getFornecedor() != null)
+			posicionaFornecedor(TelaPesquisaController.getFornecedor());
+	}
+
+	public Fornecedor montaFornecedor() {
+		Fornecedor fornecedor = new Fornecedor();
+		if (!txtId.getText().isEmpty())
+			fornecedor.setId(Integer.parseInt(txtId.getText()));
+
+		if (!txtCnpj.getText().isEmpty())
+			fornecedor.setCnpj(txtCnpj.getText());
+
+		if (!txtRazaoSocial.getText().isEmpty())
+			fornecedor.setRazaoSocial(txtRazaoSocial.getText());
+
+		if (!txtTelefone.getText().isEmpty())
+			fornecedor.setTelefone(Long.parseLong(txtTelefone.getText()));
+
+		if (!txtEmail.getText().isEmpty())
+			fornecedor.setEmail(txtEmail.getText());
+		
+
+		return fornecedor;
+	}
+
+	private void posicionaFornecedor(Fornecedor fornecedor) {
+		limpaCampos();
+		
+		if(fornecedor.getCnpj()!=null&&!fornecedor.getCnpj().isEmpty())
+		txtCnpj.setText(fornecedor.getCnpj());
+		
+		if(fornecedor.getRazaoSocial()!=null&&!fornecedor.getRazaoSocial().isEmpty())
+		txtRazaoSocial.setText(fornecedor.getRazaoSocial());
+		
+		txtId.setText(Integer.toString(fornecedor.getId()));
+		
+		if(fornecedor.getEmail()!=null&&!fornecedor.getEmail().isEmpty())
+		txtEmail.setText(fornecedor.getEmail());
+		
+		if(fornecedor.getTelefone()!=null)
+		txtTelefone.setText(Long.toString(fornecedor.getTelefone()));
+	}
 	
-    @FXML
-    private TextField txtId;
+	private void limpaCampos(){
+		txtId.setText("");
+		txtCnpj.setText("");
+		txtRazaoSocial.setText("");
+		txtTelefone.setText("");
+		txtEmail.setText("");
+	}
+	
 
-    @FXML
-    private TextField txtRazaoSocial;
-
-    @FXML
-    private TextField txtTelefone;
-
-    @FXML
-    private TextField txtCnpj;
-    
-    @FXML
-    private TextField txtEmail;
-    
-    @FXML
-    void salvar(ActionEvent event) throws Exception {
-    	if(txtId.getText().isEmpty()){
-    	fornecedorDAO.inserir(montaFornecedor());
-    	new AlertaApp().start(new Stage(), "Salvo com Sucesso!");
-    	}
-    	else{
-    		fornecedorDAO.editar(montaFornecedor());
-    		new AlertaApp().start(new Stage(), "Alterado com Sucesso!");
-    	}
-    	
-    }
-
-    @FXML
-    void editar(ActionEvent event) {
-
-    }
-
-    @FXML
-    void excluir(ActionEvent event) {
-
-    }
-    
-    @FXML
-    void pesquisar(ActionEvent event) throws Exception {
-    	Stage stageTelaPesquisa = new Stage();
-    	stageTelaPesquisa.initModality(Modality.APPLICATION_MODAL);
-    	new TelaPesquisaApp().start(stageTelaPesquisa);
-    	if(TelaPesquisaController.getFornecedor() != null)
-    	posicionaFornecedor(TelaPesquisaController.getFornecedor());
-    }
-    
-    public Fornecedor montaFornecedor(){
-    	Fornecedor fornecedor = new Fornecedor();
-    	if(!txtId.getText().isEmpty())
-    	fornecedor.setId(Integer.parseInt(txtId.getText()));
-    	
-    	if(!txtCnpj.getText().isEmpty())
-    	fornecedor.setCnpj(txtCnpj.getText());
-    	
-    	if(!txtRazaoSocial.getText().isEmpty())
-    	fornecedor.setRazaoSocial(txtRazaoSocial.getText());
-    	
-    	if(!txtTelefone.getText().isEmpty())
-    	fornecedor.setTelefone(Long.parseLong(txtTelefone.getText()));
-    	
-    	if(!txtEmail.getText().isEmpty())
-    	fornecedor.setEmail(txtEmail.getText());
-    	
-    	return fornecedor;
-    }
-    
-    public void posicionaFornecedor(Fornecedor fornecedor){
-    	txtCnpj.setText(fornecedor.getCnpj());
-    	txtRazaoSocial.setText(fornecedor.getRazaoSocial());
-    	txtId.setText(Integer.toString(fornecedor.getId()));
-    	txtEmail.setText(fornecedor.getEmail());
-    	txtTelefone.setText(Long.toString(fornecedor.getTelefone()));
-    }
-    
 }
